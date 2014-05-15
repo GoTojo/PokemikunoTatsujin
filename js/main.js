e86b6c6e6e863d1a323fa8aa65eff290f96ae3dd
gotojo <MXI03066@gmail.com>
2014-05-16 07:19:15 +0900

    Added akamaru. Known issue: Akamaru behavior is strange. ToDo: Delete target circle from bgimage, it will be draw by program. Blink akamaru object while dropping and Lit object while touching collect should be separate to two objects.

--- js/main.js
+++ js/main.js
@@ -8,6 +8,28 @@ var windowHeight=480;
 var tempo=60000000/120; //BPM=120
 var fps=30;
 var frameShowWord=15;
+var wordXpos = [];
+var targets;
+(function() {
+	var wordXposLeft=12;
+	var wordXposGapWB=27;
+	var wordXposGapWW=54;
+	var idx=0;
+	var offset=wordXposLeft;
+	for (var i=0;i<7;i++) { wordXpos[idx]=offset+wordXposGapWB*idx; idx++; }
+	offset+=wordXposGapWB;
+	for (var i=0;i<5;i++) { wordXpos[idx]=offset+wordXposGapWB*idx; idx++; }
+	offset+=wordXposGapWB;
+	for (var i=0;i<7;i++) { wordXpos[idx]=offset+wordXposGapWB*idx; idx++; }
+	offset+=wordXposGapWB;
+	for (var i=0;i<5;i++) { wordXpos[idx]=offset+wordXposGapWB*idx; idx++; }
+	offset+=wordXposGapWB;
+	for (var i=0;i<3;i++) { wordXpos[idx]=offset+wordXposGapWB*idx; idx++; }
+})();
+var scoreAreaHeight=32;
+var dropStartPosY=scoreAreaHeight;
+var lastPosY=windowHeight-32;
+var dropHeight=lastPosY-dropStartPosY;
 var WordObj=Class.create(Sprite, {
 	beginframe:0,
 	endframe:0,
@@ -17,33 +39,35 @@ var WordObj=Class.create(Sprite, {
 	negibeginframe:0,
 	negidropping:false,
 	negidownstep:0,
+	num:0,
 	initialize: function(note,word,timestamp) {
 		var num=(note-5)-oct;
 		var maxnum=27;
 		while (num<0) num+=12;
 		while (num>maxnum) num-=12;
 		Sprite.call(this,32,32);
-		this.x = num*32;
+		this.x = wordXpos[num];
 		this.y = 32;
 		this.frame = word;		
 		this.image = core.assets['images/PocketMiku.png'];
 		this.opacity = 0.0;
 		this.beginframe=Math.floor((timestamp-tempo*4/1000)*core.fps/1000)+startframe;
 		var framecount=tempo*4/1000000*fps;
-		this.downstep=(windowHeight-32)/framecount;
-		this.negidownstep=(windowHeight-32)/framecount;
+		this.downstep=dropHeight/framecount;
+		this.negidownstep=dropHeight/framecount;
 		this.negi=new Sprite(12,30);
 		this.negi.image=core.assets['images/negi.png'];
 		this.negi.x = this.x+10;
 		this.negi.y = 32;
 		this.negi.opacity=0.0;
+		this.num=num;
 		core.rootScene.addChild(this.negi);
 		core.rootScene.addChild(this);
 		//console.log("beginframe:"+this.beginframe);
 	},
 	onenterframe: function() {
 		if (this.dropping) {
-			if (this.y>=windowHeight-32) {
+			if (this.y>=lastPosY) {
 				this.dropping=false;
 			} else {
 				this.y+=this.downstep;
@@ -55,6 +79,7 @@ var WordObj=Class.create(Sprite, {
 		if (core.frame==this.beginframe) {
 			//console.log("begin fire:"+core.frame);
 			this.dropping=true;
+			if (targets) targets.on(this.num);
 		}
 		if ((this.endframe!=0) && (core.frame>=this.endframe)) {
 			//console.log("end fire:"+core.frame);
@@ -67,8 +92,9 @@ var WordObj=Class.create(Sprite, {
 			this.negidropping=true;
 		}
 		if (this.negidropping) {
-			if (this.negi.y>=windowHeight-32) {
+			if (this.negi.y>=lastPosY) {
 				this.negidropping=false;
+				if (targets) targets.off(this.num);
 			} else {
 				this.negi.y+=this.negidownstep;
 			}
@@ -86,7 +112,66 @@ var WordObj=Class.create(Sprite, {
 		core.rootScene.removeChild(this.negi);
 	}
 });
-
+var targetObj=Class.create(Sprite, {
+	onCount:0,
+	triggerd:false,
+	blinkstate:false,
+	targetframe:0,
+	initialize: function(x,y) {
+		Sprite.call(this,32,32);
+		this.x = x;
+		this.y = y;
+		this.image = core.assets['images/akamaru.png'];
+		this.opacity = 0.0;
+		this.onCount=0;
+		this.triggered=false;
+	},
+	trigger: function(f) {
+		this.triggered=f;
+		this.opacity=1.0;
+	},
+	on: function() {
+		this.onCount++;
+		this.opacity=1.0;
+		this.targetframe=core.frame+tempo/1000000*fps/2;
+	},
+	off: function() {
+		if (this.onCount) {
+			this.onCount--;
+		} else {
+			this.opacity=0.0;
+		}
+	},
+	onenterframe: function() {
+		if (!this.triggered&&this.onCount) {
+			if (core.frame>this.targetframe) {
+				this.blinkstate!=this.blinkstate;
+				this.opacity=this.blinkstate?1.0:0.0;
+				this.targetframe+=tempo/1000000*fps/2;
+			}
+		}
+	}
+});
+var targetObjs=Class.create(Surface, {
+	target:[],
+	interval:0,
+	initialize: function() {
+		Surface.call(this,windowWidth,windowHeight);
+		for (var i=0;i<wordXpos.length;i++) {
+			this.target[i]=new targetObj(wordXpos[i],lastPosY);
+			core.rootScene.addChild(this.target[i]);			
+		}
+	},
+	trigger: function(num,f) {
+		this.target[num].trigger(f);
+	},
+	on: function(num) {
+		this.target[num].on();
+	},
+	off: function(num) {
+		this.target[num].off();
+	}
+});
 var StartLogo=Class.create(Sprite, {
 	shown:false,
 	initialize: function(f) {
@@ -115,6 +200,7 @@ window.onload = function() {
 	core.preload('images/PocketMiku.png');
 	core.preload('images/start.png');
 	core.preload('images/negi.png');
+	core.preload('images/akamaru.png');
 	core.preload('images/PocketMikuBG.png');
 	var isStart=false;
 
@@ -133,6 +219,7 @@ window.onload = function() {
 		var bgimage= new Sprite(windowWidth,windowHeight);
 		bgimage.image=core.assets['images/PocketMikuBG.png'];
 		core.rootScene.addChild(bgimage);
+		targets=new targetObjs();
 		var startLogo = new StartLogo(startFunction);
 	}
 	core.start();
